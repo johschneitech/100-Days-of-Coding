038964e58f0e43761c76d63d64f2c9d4ecc5f1dd
Johnny Schneider Antoine <64368415+johschneitech@users.noreply.github.com>
2021-09-17 14:11:57 -0400

    Create day-3-start.py

---
+++ Day-3/day-3-start.py
@@ -0,0 +1,8 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+  print('You can ride the rollecoster!!!')
+else:
+  print("You can't ride the rollercoster.")
+  
