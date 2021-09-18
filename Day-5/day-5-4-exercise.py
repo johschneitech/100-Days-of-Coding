7e6391cfecc4787a9eb6042a766a784fc0eb386f
Johnny Schneider Antoine <64368415+johschneitech@users.noreply.github.com>
2021-09-18 05:35:32 -0400

    Create day-5-4-exercise.py

---
+++ Day-5/day-5-4-exercise.py
@@ -0,0 +1,10 @@
+#Write your code below this row 👇
+for number in range(1, 101):
+  if number % 3 ==0:
+    print("Fizz")
+  if number % 5 ==0:
+    print("Buzz")
+  if number % 3 ==0 and number % 5 == 0:
+    print("Fizz Buzz")
+  else:
+    print(number)
