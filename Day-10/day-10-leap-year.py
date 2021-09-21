4a1441e32d143433d4d030970bccd03d5cd57c39
Johnny Schneider Antoine <64368415+johschneitech@users.noreply.github.com>
2021-09-21 06:03:34 -0400

    Create day-10-leap-year.py

---
+++ Day-10/day-10-leap-year.py
@@ -0,0 +1,31 @@
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        print(f"{year} is a leap year.")
+        return True
+      else:
+        print(f"{year} is NOT a leap year.")
+        return False
+    else:
+      print(f"{year} is a leap year.")
+      return True
+    print(f"{year} is NOT a leap year.")
+    return False
+  else:
+    print(f"{year} is NOT a leap year.")
+    return False
+
+def days_in_month(year, month):
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  if is_leap(year) and month == 2:
+    return 29
+  return  month_days[month -1]
+  
+  
+#🚨 Do NOT change any of the code below 
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
+
