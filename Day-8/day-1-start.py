cc67ab659da764f611ab9b62fcf4619ee0b16ef0
Johnny Schneider Antoine <64368415+johschneitech@users.noreply.github.com>
2021-09-21 05:30:45 -0400

    Create day-1-start.py

---
+++ Day-8/day-1-start.py
@@ -0,0 +1,12 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+def greet():
+  print("What's up?")
+  print("Are you alright?")
+  print("How are you doing?")
+
+
+greet()
